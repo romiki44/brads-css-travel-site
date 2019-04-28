8eeab6c667132b5b0907308228c92f9def786b5a
Roman Kiss <romiki44@hotmail.com>
2019-04-28 11:09:24 +0200

    responsive images with info in images

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,6 +1,7 @@
 const gulp=require('gulp');
 const postcss=require('gulp-postcss');
 const autoprefixer=require('autoprefixer');
+const mixins=require('postcss-mixins');
 const cssvars=require('postcss-simple-vars');
 const nested=require('postcss-nested');
 const cssimport=require('postcss-import');
@@ -8,7 +9,7 @@ const cssimport=require('postcss-import');
 gulp.task('styles', ()=>{
     //console.log('css touched...');
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
         .on('error', function(error) {
             console.log('error css: ', error.toString());
             this.emit('end');
