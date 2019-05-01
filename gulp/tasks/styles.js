606f674de41fac3dd96d66fb3170dd11568dc75c
Roman Kiss <romiki44@hotmail.com>
2019-05-01 16:30:46 +0200

    Header section completed

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,11 +5,12 @@ const mixins=require('postcss-mixins');
 const cssvars=require('postcss-simple-vars');
 const nested=require('postcss-nested');
 const cssimport=require('postcss-import');
+const hexrgba=require('postcss-hexrgba');
 
 gulp.task('styles', ()=>{
     //console.log('css touched...');
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
         .on('error', function(error) {
             console.log('error css: ', error.toString());
             this.emit('end');
